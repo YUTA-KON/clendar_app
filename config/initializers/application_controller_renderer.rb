4f4076029f9ceb12b8a79a3ffb3d6229da3a1d2f
YUTA-KON <dorata.1007@gmail.com>
2021-09-30 17:36:16 +0900

    cant see other users calendar

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0e8a23e064aae663962bd7d90a4a7be4cc11197232314aa275fb53a29a9d2387c409a9c6aedcf868d2b5144beb0fa1d5d675a633eb47de01c47e0ba7799bca30'
