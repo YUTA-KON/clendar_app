c47ee44f132beafd4d97ebc886786bc56cfef2a2
KONYUTA <dorata.1007@gmail.com>
2021-11-18 13:57:47 +0000

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery-ui
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 //
-//= require jquery
-//= require jquery-ui
 //= require moment
 //= require fullcalendar
 //= require popper
